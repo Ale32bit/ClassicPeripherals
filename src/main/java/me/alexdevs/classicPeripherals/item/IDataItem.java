97c46bcb7d323717eaca5a75d030be1a608c9b68
Alessandro Proto <alex@alexdevs.me>
2025-12-06 17:30:09 +0100

    Add migration mechanism from NBT to Components

--- src/main/java/me/alexdevs/classicPeripherals/item/IDataItem.java
+++ src/main/java/me/alexdevs/classicPeripherals/item/IDataItem.java
@@ -2,6 +2,7 @@ package me.alexdevs.classicPeripherals.item;
 
 import me.alexdevs.classicPeripherals.ModComponents;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 
@@ -9,6 +10,8 @@ import java.util.Optional;
 
 public interface IDataItem {
     static Optional<String> getData(ItemStack stack) {
+        tryMigrate(stack);
+
         var data = stack.getComponents().get(ModComponents.NFC_DATA);
         if (data == null || data.isEmpty()) {
             return Optional.empty();
@@ -43,5 +46,60 @@ public interface IDataItem {
 
     static int getColor(ItemStack stack) {
         return 0xFF_000000 | stack.getComponents().getOrDefault(ModComponents.NFC_COLOR, 0xFFFFFF);
+    }
+
+    static boolean tryMigrate(ItemStack stack) {
+        if (stack.has(ModComponents.NFC_DATA)) {
+            return false;
+        }
+
+        var legacyData = DataItemData.migrate(stack);
+        if (legacyData.isEmpty()) {
+            return false;
+        }
+
+        setData(stack, legacyData.get().data());
+        setReadOnly(stack, legacyData.get().readonly());
+        setColor(stack, legacyData.get().color());
+
+        return true;
+    }
+
+    record DataItemData(String data, boolean readonly, int color) {
+        public static Optional<DataItemData> migrate(ItemStack stack) {
+            if (!(stack.getItem() instanceof IDataItem)) {
+                return Optional.empty();
+            }
+
+            if (!stack.has(DataComponents.CUSTOM_DATA)) {
+                return Optional.empty();
+            }
+
+            var customData = stack.get(DataComponents.CUSTOM_DATA);
+            if (customData == null || customData.isEmpty()) {
+                return Optional.empty();
+            }
+
+            String data;
+            boolean readonly = false;
+            int color = 0xFFFFFF;
+
+            var tag = customData.copyTag();
+            if (!tag.contains("data", Tag.TAG_STRING)) {
+                return Optional.empty();
+            }
+
+            data = tag.getString("data");
+
+            if (tag.contains("readOnly", Tag.TAG_BYTE)) {
+                readonly = tag.getBoolean("readOnly");
+            }
+
+            if (tag.contains("color", Tag.TAG_INT)) {
+                color = tag.getInt("color");
+            }
+
+            return Optional.of(new DataItemData(data, readonly, color));
+        }
     }
 }
