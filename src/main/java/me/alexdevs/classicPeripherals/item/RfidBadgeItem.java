cbbc1ad8068ef5f08afd2dd12fe2a19af2febef7
Alessandro Proto <alex@alexdevs.me>
2026-05-20 17:01:17 +0200

    Add hover text to RfidBadgeItem displaying UUID in advanced tooltip mode

--- src/main/java/me/alexdevs/classicPeripherals/item/RfidBadgeItem.java
+++ src/main/java/me/alexdevs/classicPeripherals/item/RfidBadgeItem.java
@@ -4,6 +4,8 @@ import dev.emi.trinkets.api.TrinketsApi;
 import me.alexdevs.classicPeripherals.core.ItemDataHandler;
 import me.alexdevs.classicPeripherals.integrations.TrinketsIntegration;
 import me.alexdevs.classicPeripherals.mixinInterface.ILivingEntityMixin;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -11,8 +13,10 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Optional;
 
 public class RfidBadgeItem extends Item implements IDataItem {
@@ -73,5 +77,13 @@ public class RfidBadgeItem extends Item implements IDataItem {
     @Override
     public boolean supportsPrivateKey() {
         return false;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> components, TooltipFlag tooltipFlag) {
+        if (tooltipFlag.isAdvanced()) {
+            var id = ItemDataHandler.getId(stack);
+            id.ifPresent(uuid -> components.add(Component.literal(uuid.toString()).withStyle(ChatFormatting.GRAY)));
+        }
     }
 }
