35d129d4703f82e55415f9ea1d580ffa888de735
Alessandro Proto <alex@alexdevs.me>
2026-05-20 13:30:03 +0200

    Add ECDH support with public key derivation and shared secret computation, update key validation methods

--- src/main/java/me/alexdevs/classicPeripherals/utils/LuaUtils.java
+++ src/main/java/me/alexdevs/classicPeripherals/utils/LuaUtils.java
@@ -3,9 +3,15 @@ package me.alexdevs.classicPeripherals.utils;
 import dan200.computercraft.api.lua.LuaException;
 
 public class LuaUtils {
-    public static void validateKey(String privateKey) throws LuaException {
-        if (privateKey != null && privateKey.length() != 32) {
+    public static void validateKey(String key) throws LuaException {
+        if (key != null && key.length() != 32) {
             throw new LuaException("Invalid key length, expected 32 bytes.");
+        }
+    }
+
+    public static void validateSignature(String key) throws LuaException {
+        if (key != null && key.length() != 64) {
+            throw new LuaException("Invalid signature length, expected 64 bytes.");
         }
     }
 }
